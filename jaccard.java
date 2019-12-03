3708a4d2db2ed7887066850f53acd7e09f83004e
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2019-12-03 20:25:51 +0000

    Add files via upload
    o ficheiro bd.txt tem de estar no mesmo diretório da /src

--- jaccard.java
+++ jaccard.java
@@ -1,9 +1,20 @@
 package projeto;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class jaccard {
-
+	static ArrayList<String> users;
+	static ArrayList<String> itens;
+	static Map<String, ArrayList<String>> tabela = new Hashtable<>();
+	
 	public static double simJaccard(ArrayList<Integer> c1, ArrayList<Integer> c2) {
 		double intercecao = 0;
 		int tamanho = c1.size();
@@ -24,7 +35,7 @@ public class jaccard {
 		return res;
 	}
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		ArrayList<Integer> l1 = new ArrayList<>();
 		ArrayList<Integer> l2 = new ArrayList<>();
 		l1.add(1);
@@ -42,6 +53,29 @@ public class jaccard {
 		double similaridade = simJaccard(l1, l2);
 		double distancia = 1-similaridade;
 		System.out.printf("Similaridade: %f\nDiferença: %f\n",similaridade, distancia);
+		ArrayList<String> bd = new ArrayList<>();
+		readDB("bd.txt");
+		
+		
+		//System.out.println(tabela.get("UserA"));
 	}
+	
+	public static void readDB(String filename) throws IOException {
+		Path currentRelativePath = Paths.get("");
+		String s = currentRelativePath.toAbsolutePath().toString();
+		File file = new File(s+"/src/"+filename);
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String st;
+		while ((st = br.readLine()) != null) {
+			String[] obj = st.split("->");
+			if(tabela.containsKey(obj[0])==false) {
+				tabela.put(obj[0], new ArrayList<>());
+				tabela.get(obj[0]).add(obj[1]);
+			}else{
+				tabela.get(obj[0]).add(obj[1]);
+			}
+		}
+	}
+	
 
 }
