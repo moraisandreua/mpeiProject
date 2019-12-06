7f365363319f3430681db3b71d673063b4908732
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2019-12-06 19:41:12 +0000

    Add files via upload

---
+++ matlab/main.m
@@ -0,0 +1,29 @@
+%% Histogram 0
+a0=csvread('hist0.csv');
+figure(100);
+hist(a0);
+
+%% Histogram 1
+a1=csvread('hist1.csv');
+figure(101);
+hist(a1);
+
+%% Histogram 2
+a2=csvread('hist2.csv');
+figure(102);
+hist(a2);
+
+%% Histogram 3
+a3=csvread('hist3.csv');
+figure(103);
+hist(a3);
+
+%% Histogram 4
+a4=csvread('hist4.csv');
+figure(104);
+hist(a4);
+
+%% Histogram 5
+a5=csvread('hist5.csv');
+figure(105);
+hist(a5);
